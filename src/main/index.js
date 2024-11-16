2a00db3d76603ab3613c4dc10af420dc34afd270
Auzmit <atrudkov@mail.ru>
2024-11-16 12:52:43 +0300

    check ssh,.. access...

--- src/main/index.js
+++ src/main/index.js
@@ -9,7 +9,7 @@ function createWindow() {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 900,
-    height: 670,
+    height: 680,
     show: false,
     autoHideMenuBar: true,
     ...(process.platform === 'linux' ? { icon } : {}),
